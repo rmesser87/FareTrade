be74a1162ae8db721fa1cc64ba30e6c0120bc8fa
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-05-14 00:08:29 -0500

    adding handlebars templating to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
@@ -14,6 +15,11 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
+
+// Set Handebars as the default templating engine.
+// =============================================================
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Static directory to be served
 app.use(express.static("app/public"));
