b13dccaf151dbe9d7e519acd107b53cfa8f3e48d
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-05-15 23:37:30 -0500

    forgot to save the server.js file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express')
-var app = express()
-var passport = require('passport')
-var session = require('express-session')
-var bodyParser = require('body-parser')
-var env = require('dotenv').load()
-var exphbs = require('express-handlebars')
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars');
  
  
 //For BodyParser
@@ -25,7 +25,7 @@ app.use(passport.session()); // persistent login sessions
  
  
 //For Handlebars
-app.set('views', './app/views')
+app.set('views', './app/views');
 app.engine('hbs', exphbs({
     extname: '.hbs'
 }));
@@ -56,12 +56,12 @@ require('./app/config/passport/passport.js')(passport, models.user);
  
 models.sequelize.sync().then(function() {
  
-    console.log('Nice! Database looks fine')
+    console.log('Nice! Database looks fine');
  
  
 }).catch(function(err) {
  
-    console.log(err, "Something went wrong with the Database Update!")
+    console.log(err, "Something went wrong with the Database Update!");
  
 });
  
@@ -72,6 +72,6 @@ app.listen(5000, function(err) {
  
         console.log("Site is live");
          
-    else console.log(err)
+    else console.log(err);
  
 });
