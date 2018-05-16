6e52bd98d3835aca5414bf1ba93b6f97702ed5a8
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-05-15 23:31:00 -0500

    Passport.js now working

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,6 +3,7 @@ var authController = require('../controllers/authcontroller.js');
  
 module.exports = function(app, passport) {
  
+ 
     app.get('/signup', authController.signup);
  
  
@@ -18,5 +19,30 @@ module.exports = function(app, passport) {
     ));
  
  
+    app.get('/dashboard', isLoggedIn, authController.dashboard);
  
-}
+ 
+ 
+    app.get('/logout', authController.logout);
+ 
+ 
+    app.post('/signin', passport.authenticate('local-signin', {
+            successRedirect: '/dashboard',
+ 
+            failureRedirect: '/signin'
+        }
+ 
+    ));
+ 
+ 
+    function isLoggedIn(req, res, next) {
+ 
+        if (req.isAuthenticated())
+ 
+            return next();
+ 
+        res.redirect('/signin');
+ 
+    }
+ 
+};
