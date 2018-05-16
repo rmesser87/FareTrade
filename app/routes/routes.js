0d35a81d8802c8957b3ce6d94fe4934ffa935575
grantposell <grantposell@gmail.com>
2018-05-15 20:39:33 -0500

    Added more passport information

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,16 +1,16 @@
 
-// Requiring our models
+// // Requiring our models
 var db = require("../models");
 
-// Routes
-// =============================================================
+// // Routes
+// // =============================================================
 module.exports = function(app) {
     // GET route for getting all of the items
 
     app.get("/api/items", function(req, res) {
-        // findAll returns all entries for a table when used with no options
+//         // findAll returns all entries for a table when used with no options
         db.Todo.findAll({}).then(function(dbTodo) {
-          // We have access to the todos as an argument inside of the callback function
+//           // We have access to the todos as an argument inside of the callback function
           res.json(dbTodo);
         });
     });
