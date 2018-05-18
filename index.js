13221e8ef903a370a8b74f6845c4f384268fc139
ar0vi <33821453+ar0vi@users.noreply.github.com>
2018-05-17 19:31:17 -0500

    commented out server
    commented out

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var app = express();  
-app.use(express.static("public"));
+// var express = require('express');
+// var app = express();  
+// app.use(express.static("public"));
 
-var handlebars = require("express-handlebars")
-.create({defaultLayout: 'main'});
+// var handlebars = require("express-handlebars")
+// .create({defaultLayout: 'main'});
 
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req,res){
-	res.render('home');
-});
+// app.get('/', function(req,res){
+// 	res.render('home');
+// });
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', handlebars.engine);
+// app.set('view engine', 'handlebars');
 
-app.use(function(req,res){
-	res.status(404);
-	res.render('404');
-});
+// app.use(function(req,res){
+// 	res.status(404);
+// 	res.render('404');
+// });
 
-app.listen(app.get('port'), function(){
-	console.log( 'Express Server Started on http://localhost:3000');
-});
+// app.listen(app.get('port'), function(){
+// 	console.log( 'Express Server Started on http://localhost:3000');
+// });
