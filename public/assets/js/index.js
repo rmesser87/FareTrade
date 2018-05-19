3f480dd939eae7b11c8f2aadaaf2b9577f1939ad
grantposell <grantposell@gmail.com>
2018-05-19 10:04:53 -0500

    Fixed some routes on signin

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     //Click event listener for signup button on the index page
     $("#signup").on("click", function (event) {
-        location.href = ('./signup.html');
+        location.href = ('./signup');
     });
     //Click event listener for the signin button on the index page
     $("#signin").on("click", function (event) {
-        location.href = ('./signin.html');
+        location.href = ('/signin');
     });
 });
