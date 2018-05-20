50b561c36cab9517739979203239ffec5f4c4b14
grantposell <grantposell@gmail.com>
2018-05-20 18:53:06 -0500

    signin works

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
     //Click event listener for signup button on the index page
-    $("#signup").on("click", function (event) {
-        event.preventDefault();
-        location.href = ('./signup');
-    });
-    //Click event listener for the signin button on the index page
-    $("#signin").on("click", function (event) {
-        event.preventDefault();
-        location.href = ('/signin');
-    });
+    // $("#signup").on("click", function (event) {
+    //     event.preventDefault();
+    //     location.href = ('./signup');
+    // });
+    // //Click event listener for the signin button on the index page
+    // $("#signin").on("click", function (event) {
+    //     event.preventDefault();
+    //     location.href = ('/signin');
+    // });
     $("#signupform").on("click", function (event) {
+        event.preventDefault();
+    });
+    $("#signinform").on("click", function (event) {
         event.preventDefault();
     });
 });
